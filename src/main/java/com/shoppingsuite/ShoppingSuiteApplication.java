735a8456ea1472b96a7902dddb035c0e09cbadc2
leonardevans <lemutugi@gmail.com>
2022-01-28 06:32:27 -0500

    Init

---
+++ src/main/java/com/shoppingsuite/ShoppingSuiteApplication.java
@@ -0,0 +1,13 @@
+package com.shoppingsuite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingSuiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingSuiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shoppingsuite/ShoppingSuiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingsuite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingSuiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
