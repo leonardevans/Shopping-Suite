118cb85504805613c1c1ba9f69005f751eee2574
leonardevans <lemutugi@gmail.com>
2022-02-05 09:21:30 -0500

    ApplicationContextProvider

--- src/main/java/com/shoppingsuite/ShoppingSuiteApplication.java
+++ src/main/java/com/shoppingsuite/ShoppingSuiteApplication.java
@@ -1,10 +1,20 @@
 package com.shoppingsuite;
 
+import com.shoppingsuite.config.AppProperties;
+import com.shoppingsuite.config.ApplicationContextProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class ShoppingSuiteApplication {
+
+    @Bean
+    public static ApplicationContextProvider contextProvider() {
+        return new ApplicationContextProvider();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ShoppingSuiteApplication.class, args);
