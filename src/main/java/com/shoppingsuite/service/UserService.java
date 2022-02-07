c91f65e6337cc5bb91f89d737c1cb688e1e8fdd1
leonardevans <lemutugi@gmail.com>
2022-02-07 06:03:19 -0500

    UserService CRUD

--- src/main/java/com/shoppingsuite/service/UserService.java
+++ src/main/java/com/shoppingsuite/service/UserService.java
@@ -9,4 +9,7 @@ public interface UserService {
     Optional<User> getUserById(Long id);
     Optional<User> getUserByEmail(String email);
     User saveUser(UserDto userDto);
+    User saveUser(User user);
+    boolean deleteUser(User user);
+    boolean deleteUserById(Long id);
 }
--- src/main/java/com/shoppingsuite/service/impl/UserServiceImpl.java
+++ src/main/java/com/shoppingsuite/service/impl/UserServiceImpl.java
@@ -1,4 +1,69 @@
 package com.shoppingsuite.service.impl;
 
-public class UserServiceImpl {
+import com.shoppingsuite.persistence.dao.RoleRepo;
+import com.shoppingsuite.persistence.dao.UserRepo;
+import com.shoppingsuite.persistence.model.User;
+import com.shoppingsuite.service.UserService;
+import com.shoppingsuite.web.dto.UserDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    UserRepo userRepo;
+
+    @Autowired
+    RoleRepo roleRepo;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Override
+    public Optional<User> getUserById(Long id) {
+        return userRepo.findById(id);
+    }
+
+    @Override
+    public Optional<User> getUserByEmail(String email) {
+        return userRepo.findByEmail(email);
+    }
+
+    @Override
+    public User saveUser(UserDto userDto) {
+        userDto.setPassword(passwordEncoder.encode(userDto.getPassword()));
+        //create new user
+        User user = new User(userDto);
+        return userRepo.save(user);
+    }
+
+    @Override
+    public User saveUser(User user) {
+        return userRepo.save(user);
+    }
+
+    @Override
+    public boolean deleteUser(User user) {
+        try{
+            userRepo.delete(user);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean deleteUserById(Long id) {
+        try{
+            userRepo.deleteById(id);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return false;
+        }
+        return true;
+    }
 }
