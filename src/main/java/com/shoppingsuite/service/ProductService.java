26efcb08179ca7fe4f4fcea08666b2e7a1ab62d9
leonardevans <lemutugi@gmail.com>
2022-02-14 07:45:53 -0500

    ProductService methods

--- src/main/java/com/shoppingsuite/service/ProductService.java
+++ src/main/java/com/shoppingsuite/service/ProductService.java
@@ -1,4 +1,16 @@
 package com.shoppingsuite.service;
 
+import com.shoppingsuite.persistence.model.Product;
+import com.shoppingsuite.persistence.model.ProductCategory;
+
+import java.util.Optional;
+import java.util.Set;
+
 public interface ProductService {
+    Product save(Product product);
+    Optional<Product> getById(Long id);
+    boolean delete(Product product);
+    boolean deleteById(Long id);
+    Set<Product> getByProductCategory(ProductCategory productCategory);
+    Set<Product> getByProductCategoryId(Long id);
 }
