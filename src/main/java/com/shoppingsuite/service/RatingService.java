2511b1ac4fa86822fb69f80f2f16d885bdac95c2
leonardevans <lemutugi@gmail.com>
2022-02-14 07:41:24 -0500

    RatingService methods

--- src/main/java/com/shoppingsuite/service/RatingService.java
+++ src/main/java/com/shoppingsuite/service/RatingService.java
@@ -1,4 +1,14 @@
 package com.shoppingsuite.service;
 
+import com.shoppingsuite.persistence.model.Rating;
+
+import java.util.Optional;
+import java.util.Set;
+
 public interface RatingService {
+    Rating saveRating(Rating review);
+    Optional<Rating> getById(Long id);
+    boolean deleteById(Long id);
+    boolean delete(Rating review);
+    Set<Rating> getByProductId(Long productId);
 }
