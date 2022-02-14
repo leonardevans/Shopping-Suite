46b305ab8c1b937f11bc7af99160c9245476a6e3
leonardevans <lemutugi@gmail.com>
2022-02-14 08:11:41 -0500

    DealService methods

--- src/main/java/com/shoppingsuite/service/DealService.java
+++ src/main/java/com/shoppingsuite/service/DealService.java
@@ -1,4 +1,17 @@
 package com.shoppingsuite.service;
 
+import com.shoppingsuite.persistence.model.Deal;
+
+import java.util.Date;
+import java.util.Optional;
+import java.util.Set;
+
 public interface DealService {
+    Deal save(Deal deal);
+    Optional<Deal> getById(Long id);
+    boolean delete(Deal deal);
+    boolean deleteById(Long id);
+    Set<Deal> getAll(int pageNo, int pageSize);
+    Set<Deal> getAllByStartDate(int pageNo, int pageSize, Date startDate);
+    Set<Deal> getAllByEndDate(int pageNo, int pageSize, Date endDate);
 }
