0f5b52ef9d8075d82e790816174dffd77733310f
leonardevans <lemutugi@gmail.com>
2022-02-14 08:33:21 -0500

    ReviewServiceImpl  methods implemented

--- src/main/java/com/shoppingsuite/persistence/dao/ReviewRepo.java
+++ src/main/java/com/shoppingsuite/persistence/dao/ReviewRepo.java
@@ -6,8 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface ReviewRepo extends JpaRepository<Review, Long> {
-    List<Review> findAllByProduct(Product product);
+    Set<Review> findAllByProduct_Id(Long productId);
 }
--- src/main/java/com/shoppingsuite/service/impl/ReviewServiceImpl.java
+++ src/main/java/com/shoppingsuite/service/impl/ReviewServiceImpl.java
@@ -1,4 +1,53 @@
 package com.shoppingsuite.service.impl;
 
-public class ReviewServiceImpl {
+import com.shoppingsuite.persistence.dao.ReviewRepo;
+import com.shoppingsuite.persistence.model.Review;
+import com.shoppingsuite.service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+public class ReviewServiceImpl implements ReviewService {
+    @Autowired
+    ReviewRepo reviewRepo;
+
+    @Override
+    public Review save(Review review) {
+        return reviewRepo.save(review);
+    }
+
+    @Override
+    public Optional<Review> getById(Long id) {
+        return reviewRepo.findById(id);
+    }
+
+    @Override
+    public boolean deleteById(Long id) {
+        try{
+            reviewRepo.deleteById(id);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean delete(Review review) {
+        try{
+            reviewRepo.delete(review);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public Set<Review> getByProductId(Long productId) {
+        return reviewRepo.findAllByProduct_Id(productId);
+    }
 }
