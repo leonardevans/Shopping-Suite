9ca2080d4a32fd768fb34e02bd25b88ee2b68d4a
leonardevans <lemutugi@gmail.com>
2022-02-14 07:41:07 -0500

    ReviewService methods

--- src/main/java/com/shoppingsuite/service/ReviewService.java
+++ src/main/java/com/shoppingsuite/service/ReviewService.java
@@ -1,4 +1,15 @@
 package com.shoppingsuite.service;
 
+import com.shoppingsuite.persistence.model.Review;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
 public interface ReviewService {
+    Review save(Review review);
+    Optional<Review> getById(Long id);
+    boolean deleteById(Long id);
+    boolean delete(Review review);
+    Set<Review> getByProductId(Long productId);
 }
