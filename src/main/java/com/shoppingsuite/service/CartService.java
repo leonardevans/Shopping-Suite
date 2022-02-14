c0c83803cc3c7bf851196aa90cd67f3af488ef7e
leonardevans <lemutugi@gmail.com>
2022-02-14 08:28:02 -0500

    CartService methods

--- src/main/java/com/shoppingsuite/service/CartService.java
+++ src/main/java/com/shoppingsuite/service/CartService.java
@@ -1,4 +1,13 @@
 package com.shoppingsuite.service;
 
+import com.shoppingsuite.persistence.model.Cart;
+import com.shoppingsuite.persistence.model.User;
+
+import java.util.Optional;
+
 public interface CartService {
+    Cart save(Cart deal);
+    boolean delete(Cart cart);
+    boolean deleteById(Long id);
+    Optional<Cart> getByUserAndOrdered(User user, boolean ordered);
 }
