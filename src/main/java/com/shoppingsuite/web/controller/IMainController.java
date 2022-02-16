05799cd6a0dace9fe9489f4cf19667772a693763
leonardevans <lemutugi@gmail.com>
2022-02-16 14:45:14 -0500

    IMainController interface

---
+++ src/main/java/com/shoppingsuite/web/controller/IMainController.java
@@ -0,0 +1,11 @@
+package com.shoppingsuite.web.controller;
+
+public interface IMainController {
+    public String showLoginPage();
+    public String showIndexPage();
+    public String showProductPage();
+    public String showDealsPage();
+    public String showDealPage();
+    public String showCartPage();
+    public String showCheckoutPage();
+}
