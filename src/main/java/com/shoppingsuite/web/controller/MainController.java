5f03e37562979e8da595f826d6c8ff5c2dca3e42
leonardevans <lemutugi@gmail.com>
2022-02-19 19:02:54 -0500

    IMainController interface implemented

--- src/main/java/com/shoppingsuite/web/controller/MainController.java
+++ src/main/java/com/shoppingsuite/web/controller/MainController.java
@@ -6,15 +6,41 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MainController {
-    //show login page
+public class MainController implements IMainController{
+    @Override
     @GetMapping("/login")
-    public String login(){
+    public String showLoginPage() {
         return "login";
     }
 
+    @Override
     @GetMapping("/")
-    public String showIndexPage(Model model){
+    public String showIndexPage() {
         return "index";
+    }
+
+    @Override
+    public String showProductPage() {
+        return null;
+    }
+
+    @Override
+    public String showDealsPage() {
+        return null;
+    }
+
+    @Override
+    public String showDealPage() {
+        return null;
+    }
+
+    @Override
+    public String showCartPage() {
+        return null;
+    }
+
+    @Override
+    public String showCheckoutPage() {
+        return null;
     }
 }
