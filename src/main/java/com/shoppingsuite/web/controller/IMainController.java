e48480bf61957cb7b3a43e4cdbc5817caa5e053c
leonardevans <lemutugi@gmail.com>
2022-03-03 15:45:03 -0500

    MainController.showIndexPage - Index Page products and categories

--- src/main/java/com/shoppingsuite/web/controller/IMainController.java
+++ src/main/java/com/shoppingsuite/web/controller/IMainController.java
@@ -1,11 +1,19 @@
 package com.shoppingsuite.web.controller;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 public interface IMainController {
     public String showLoginPage();
-    public String showIndexPage();
+    @GetMapping("/")
+    String showIndexPage(Model model);
+
     public String showProductPage();
     public String showDealsPage();
     public String showDealPage();
     public String showCartPage();
     public String showCheckoutPage();
+
+    @GetMapping("page/{pageNo}")
+    String findPaginated(int pageNo, String sortField, String sortDir, Model model, String returnPage);
 }
--- src/main/java/com/shoppingsuite/web/controller/impl/MainController.java
+++ src/main/java/com/shoppingsuite/web/controller/impl/MainController.java
@@ -1,18 +1,23 @@
 package com.shoppingsuite.web.controller.impl;
 
+import com.shoppingsuite.persistence.model.Product;
 import com.shoppingsuite.security.AuthUtil;
+import com.shoppingsuite.service.ProductCategoryService;
+import com.shoppingsuite.service.ProductService;
 import com.shoppingsuite.web.controller.IMainController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController implements IMainController {
@@ -21,6 +26,12 @@ public class MainController implements IMainController {
 
     @Autowired
     AuthUtil authUtil;
+
+    @Autowired
+    ProductService productService;
+
+    @Autowired
+    ProductCategoryService productCategoryService;
 
     @Override
     @GetMapping("/login")
@@ -47,8 +58,9 @@ public class MainController implements IMainController {
 
     @Override
     @GetMapping("/")
-    public String showIndexPage() {
-        return "index";
+    public String showIndexPage(Model model) {
+        model.addAttribute("productCategories", productCategoryService.getAll());
+        return findPaginated(1, "name", "asc",  model, "/index");
     }
 
     @Override
@@ -74,5 +86,26 @@ public class MainController implements IMainController {
     @Override
     public String showCheckoutPage() {
         return null;
+    }
+
+    @GetMapping("page/{pageNo}")
+    @Override
+    public String findPaginated(int pageNo, String sortField, String sortDir, Model model, String returnPage) {
+        int pageSize = 10;
+
+        Page<Product> page = productService.getAll(pageNo, pageSize, sortField, sortDir);
+        List<Product> products = page.getContent();
+
+        model.addAttribute("pageSize", pageSize);
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("totalItems", page.getTotalElements());
+
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
+
+        model.addAttribute("products", products);
+        return returnPage;
     }
 }
