0bae36facc30d16838719949f3e313b10d92d7a2
leonardevans <lemutugi@gmail.com>
2022-03-07 16:33:05 -0500

    index/home page show categories

--- src/main/java/com/shoppingsuite/service/impl/ProductServiceImpl.java
+++ src/main/java/com/shoppingsuite/service/impl/ProductServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-import java.util.Set;
 
 @Service
 public class ProductServiceImpl implements ProductService {
