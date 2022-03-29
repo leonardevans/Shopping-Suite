834694d656b51b9f9efa205be99c653442106845
leonardevans <lemutugi@gmail.com>
2022-03-29 04:59:24 -0400

    CartProductId Embeddable class to use in the cart_products join table

---
+++ src/main/java/com/shoppingsuite/persistence/embed/CartProductId.java
@@ -0,0 +1,41 @@
+package com.shoppingsuite.persistence.embed;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CartProductId implements Serializable {
+    @Column(name = "cart_id")
+    private Long cartId;
+
+    @Column(name = "product_id")
+    private Long productId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        CartProductId that = (CartProductId) o;
+        return Objects.equals(cartId, that.cartId) &&
+                Objects.equals(productId, that.productId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cartId, productId);
+    }
+}
