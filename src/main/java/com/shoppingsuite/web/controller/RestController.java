0330223c28a32aad4af6a23992478fba5abb2731
leonardevans <lemutugi@gmail.com>
2022-03-30 05:12:13 -0400

    addDealToCart method implemented

--- src/main/java/com/shoppingsuite/web/controller/RestController.java
+++ src/main/java/com/shoppingsuite/web/controller/RestController.java
@@ -1,18 +1,18 @@
 package com.shoppingsuite.web.controller;
 
 import com.shoppingsuite.persistence.joins.CartProduct;
-import com.shoppingsuite.persistence.model.Cart;
-import com.shoppingsuite.persistence.model.Product;
-import com.shoppingsuite.persistence.model.Review;
-import com.shoppingsuite.persistence.model.User;
+import com.shoppingsuite.persistence.model.*;
 import com.shoppingsuite.security.AuthUtil;
 import com.shoppingsuite.service.CartService;
+import com.shoppingsuite.service.DealService;
 import com.shoppingsuite.service.ProductService;
 import com.shoppingsuite.service.ReviewService;
 import com.shoppingsuite.utils.CartUtil;
+import com.shoppingsuite.web.dto.AddDealToCartDto;
 import com.shoppingsuite.web.dto.AddToCartDto;
 import com.shoppingsuite.web.dto.ReviewDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,14 +27,16 @@ public class RestController {
     private AuthUtil authUtil;
     private CartService cartService;
     private CartUtil cartUtil;
+    private DealService dealService;
 
     @Autowired
-    public RestController(ReviewService reviewService, ProductService productService, AuthUtil authUtil, CartService cartService, CartUtil cartUtil) {
+    public RestController(ReviewService reviewService, ProductService productService, AuthUtil authUtil, CartService cartService, CartUtil cartUtil, DealService dealService) {
         this.reviewService = reviewService;
         this.productService = productService;
         this.authUtil = authUtil;
         this.cartService = cartService;
         this.cartUtil = cartUtil;
+        this.dealService = dealService;
     }
 
     @PostMapping(value = "/api/make-review")
@@ -60,6 +62,34 @@ public class RestController {
 
         if (!userCart.getCartProducts().contains(product)){
             CartProduct cartProduct = new CartProduct(userCart, product, addToCartDto.getQuantity(), product.getPrice());
+            userCart.getCartProducts().add(cartProduct);
+        }
+
+        if (loggedInUser != null){
+            userCart.setUser(loggedInUser);
+
+            //save the cart
+            userCart = cartService.save(userCart);
+        }
+
+
+        //set the cart to session variable
+        httpSession.setAttribute("cart", userCart);
+
+        return ResponseEntity.ok(true);
+    }
+
+    @PostMapping(value = "/api/add-deal-to-cart")
+    public ResponseEntity addDealToCart(@Valid @RequestBody AddDealToCartDto addDealToCartDto, HttpSession httpSession) throws Exception {
+        User loggedInUser = authUtil.getLoggedInUser();
+
+        Cart userCart = cartUtil.updateCart(httpSession);
+
+        //get the deal from db
+        Deal deal = dealService.getById(addDealToCartDto.getDealId()).orElseThrow(ChangeSetPersister.NotFoundException::new);
+
+        if (!userCart.getCartProducts().contains(deal.getProduct())){
+            CartProduct cartProduct = new CartProduct(userCart, deal.getProduct(), addDealToCartDto.getQuantity(), deal.getDealPrice());
             userCart.getCartProducts().add(cartProduct);
         }
 
