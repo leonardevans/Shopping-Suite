0125609491770496d5f213f26e6a580c68e273b5
leonardevans <lemutugi@gmail.com>
2022-06-11 18:27:33 -0400

    add base url to the allowed origins

--- src/main/java/com/shoppingsuite/config/WebMvcConfig.java
+++ src/main/java/com/shoppingsuite/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.shoppingsuite.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,10 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
     private final long MAX_AGE_SECS = 3600;
 
+    @Value("${BASE_URL}")
+    private String baseUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(baseUrl)
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
